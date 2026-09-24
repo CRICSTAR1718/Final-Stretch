e426d50791b775b2b9b01a053dd3cbfe8a61afad
Adeela Azeez <adeelaazeez9@gmail.com>
2026-09-24 22:41:15 +0530

    Added solution for TEST 002 - Ques 2

--- Test 002/DSA/ques2.cpp
+++ Test 002/DSA/ques2.cpp
@@ -1,8 +1,4 @@
-#include <iostream>
-#include <cmath>
-#include <cstdio>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 int main() {
     // your code goes here
